a035f11e85414fa039d7ec83439313b06ed28d06
colourblind <tom.milsom@gmail.com>
2012-03-31 17:25:24 +0100

    Added missing file

---
+++ src/SpaghettiDetector.Test/AttributeTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace SpaghettiDetector.Test
+{
+    [TestFixture]
+    class AttributeTests : TestBase
+    {
+        [Test]
+        public void AttributeClassTarget()
+        {
+            var n = GetNode("TestAttributeClassTarget");
+            Assert.That(n.Dependencies.Where(x => x.ClassName == "TestTargetAttribute").Count() > 0);
+        }
+
+        [Test]
+        public void AttributeFieldTarget()
+        {
+            var n = GetNode("TestAttributeFieldTarget");
+            Assert.That(n.Dependencies.Where(x => x.ClassName == "TestTargetAttribute").Count() > 0);
+        }
+
+        [Test]
+        public void AttributePropertyTarget()
+        {
+            var n = GetNode("TestAttributePropertyTarget");
+            Assert.That(n.Dependencies.Where(x => x.ClassName == "TestTargetAttribute").Count() > 0);
+        }
+
+        [Test]
+        public void AttributeMethodTarget()
+        {
+            var n = GetNode("TestAttributeMethodTarget");
+            Assert.That(n.Dependencies.Where(x => x.ClassName == "TestTargetAttribute").Count() > 0);
+        }
+
+    }
+
+    #region Test Classes
+
+    [TestTarget]
+    class TestAttributeClassTarget
+    {
+        int foo { get; set; }
+    }
+
+    class TestAttributeFieldTarget
+    {
+        [TestTarget]
+        int foo;
+    }
+
+    class TestAttributePropertyTarget
+    {
+        [TestTarget]
+        int foo { get; set; }
+    }
+
+    class TestAttributeMethodTarget
+    {
+        [TestTarget]
+        int Foo()
+        {
+            return 1;
+        }
+    }
+
+    class TestTargetAttribute : Attribute
+    {
+
+    }
+
+    #endregion
+}
